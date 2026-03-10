8ffa63ed67e3cb3b06c3d7b4c528459a023ab130
Li Yijin <yijin.25@intl.zju.edu.cn>
2026-03-10 16:31:22 +0800

    add variables.py for population calculation

--- Practical 4/variables.py
+++ Practical 4/variables.py
@@ -1,1 +1,11 @@
-
+a = 5.08
+b = 5.33
+c = 5.55
+d = b - a
+e = c - b
+if d > e:
+    # The growth in the first ten years was larger, showing the population growth is decelerating
+    print("d is larger than e. Population growth is decelerating in Scotland.")
+else:
+    # # The growth in the next ten years is larger, showing the population growth is accelerating
+    print("d is smaller than e. Population growth is accelerating in Scotland.")
