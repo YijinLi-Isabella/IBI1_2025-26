34c093645035afb16a6b9b3862fc12a9607ad750
Li Yijin <yijin.25@intl.zju.edu.cn>
2026-03-15 15:52:49 +0800

    update the pseudocode

--- Practical 4/infection.py
+++ Practical 4/infection.py
@@ -6,7 +6,9 @@ growth_rate = 0.4
 total_students = 91
 days = 0
 current_infected = initial_infected
+print(initial_infected, "students are infected on day", days)
 while current_infected < total_students:
     current_infected = current_infected * 1.4
     days += 1
+    print(current_infected, "students are infected on day", days)
 print(days, "days", "were taken to infect all the students")
