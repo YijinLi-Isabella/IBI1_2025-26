58e883f613fbc086ae29400ed591169052ae8b85
Li Yijin <yijin.25@intl.zju.edu.cn>
2026-03-24 15:52:29 +0800

    Update infection.py

--- Practical 4/infection.py
+++ Practical 4/infection.py
@@ -1,13 +1,13 @@
-# start with 5 infected individuals
-# the growth rate is 40%
-# the total number of the students is 91
+# Start with 5 infected individuals
+# The growth rate is 40%
+# The total number of the students is 91
 initial_infected = 5
 growth_rate = 0.4
 total_students = 91
 days = 0
 current_infected = initial_infected
 print(initial_infected, "students are infected on day", days)
-# calculate and print the number of infected students on each day
+# Calculate and print the number of infected students on each day
 while current_infected < total_students:
     current_infected = current_infected * 1.4
     days += 1
