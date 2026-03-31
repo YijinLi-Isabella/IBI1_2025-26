1abed61bd9e1468ad634f4974baece82386a3e7c
Li Yijin <yijin.25@intl.zju.edu.cn>
2026-03-31 16:12:27 +0800

    add largest_orf.py

---
+++ Practical 7/largest_orf.py
@@ -0,0 +1,15 @@
+seq = 'AAGAUACAUGCAAGUGGUGUGUCUGUUCUGAGAGGGCCUAAAAG'
+import re
+# find all the orf
+orf_UAA = re.findall(r"(AUG.+UAA)", seq) # the start codon can begin at any position 
+orf_UAG = re.findall(r"(AUG.+UAG)", seq) # so we don't need ^
+orf_UGA = re.findall(r"(AUG.+UGA)", seq)
+orfs = orf_UAA + orf_UAG + orf_UGA
+# find the largest orf
+max_len = 0
+for orf in orfs:
+    if len(orf) > max_len:
+        max_len = len(orf)
+        max_orf = orf
+print("The largest orf is", max_orf)
+print("The length is", len(max_orf))
